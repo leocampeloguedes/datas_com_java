34be9729dabbcd7d34b15adb4048978046003357
Leonardo <Leonardo@LAPTOP-L0DNUSFL>
2021-06-19 22:56:57 -0300

    Trabalhando com Datas no JAVA 1.Date 2.Calendar

---
+++ modulo_datas/src/modulo_datas/DatasEmJava.java
@@ -0,0 +1,73 @@
+package modulo_datas;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class DatasEmJava {
+
+	public static void main(String[] args) throws ParseException {
+		// TODO Auto-generated method stub
+		
+		
+		Calendar calendar = Calendar.getInstance();
+		
+		
+		
+		/*
+		
+		Date date = new Date();
+		
+		//System.out.println(date);
+		
+		//representa a data em milisegundos
+		System.out.println("getDate() : "+date.getDate());
+		System.out.println("date.getDay() : "+date.getDay());
+		System.out.println("date.getHours() : "+date.getHours());
+		System.out.println("date.getMinutes() : "+date.getMinutes());
+		System.out.println("date.getMonth() : "+date.getMonth());
+		System.out.println("date.getSeconds() : "+date.getSeconds());
+		System.out.println("date.getTime() : "+date.getTime());
+		System.out.println("date.getTimezoneOffset() : "+date.getTimezoneOffset());
+		System.out.println("date.getYear() : "+date.getYear());
+		System.out.println("date.getYear() : "+(date.getYear()+1900));
+		System.out.println("date.getClass() : "+date.getClass());
+		
+		
+		//SIMPLE DATE FORMAT
+		
+		SimpleDateFormat simpleDate = new SimpleDateFormat("yyyy-MM-dd");  //para gravar no banco de dados "yyyy-MM-dd"
+			
+		System.out.println(simpleDate.format(date));
+		
+		simpleDate = new SimpleDateFormat("dd/MM/yyyy"); //mostrar na tela para o usuário
+		
+		System.out.println(simpleDate.format(date));
+		
+		simpleDate = new SimpleDateFormat("dd/MM/yyyy HH:mm.ss"); //mostrar na tela para o usuário com hora, minuto e segundo
+		
+		System.out.println(simpleDate.format(date));
+		
+		
+		//PARSE
+		simpleDate = new SimpleDateFormat("dd/MM/yyyy HH:mm.ss");
+		System.out.println(simpleDate.parseObject("19/06/2021 11:26.45"));
+		
+		simpleDate = new SimpleDateFormat("dd/MM/yyyy");
+		System.out.println(simpleDate.parseObject("19/06/2021"));
+		
+		
+		*/
+		
+			
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
